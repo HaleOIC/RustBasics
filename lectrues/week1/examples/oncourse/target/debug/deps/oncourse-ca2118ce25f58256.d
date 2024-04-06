70a669c71afec90c719b11f473562d89097ef2ab
HaleOIC <shinehale730@gmail.com>
2024-04-06 22:28:17 +0800

    finish lecture 1 & 2 recording and finish examples

---
+++ lectrues/week1/examples/oncourse/target/debug/deps/oncourse-ca2118ce25f58256.d
@@ -0,0 +1,5 @@
+/home/hale/UNSW_COMP6991/lectrues/week1/examples/oncourse/target/debug/deps/liboncourse-ca2118ce25f58256.rmeta: src/main.rs
+
+/home/hale/UNSW_COMP6991/lectrues/week1/examples/oncourse/target/debug/deps/oncourse-ca2118ce25f58256.d: src/main.rs
+
+src/main.rs:
---
+++ lectrues/week1/examples/oncourse/target/debug/oncourse.d
@@ -0,0 +1,1 @@
+/home/hale/UNSW_COMP6991/lectrues/week1/examples/oncourse/target/debug/oncourse: /home/hale/UNSW_COMP6991/lectrues/week1/examples/oncourse/src/main.rs
