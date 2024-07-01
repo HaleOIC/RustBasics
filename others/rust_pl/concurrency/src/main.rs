965ad6283b2dea543bf97c06b23f991f5f23b990
HaleOIC <shinehale730@gmail.com>
2024-07-01 15:15:36 +0800

    finish chapter 16 reading

---
+++ others/rust_pl/concurrency/src/main.rs
@@ -0,0 +1,138 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc::{self, channel};
+
+
+fn main(){
+    // basic case for concurrency
+    basic_case();
+
+    // use move closure with threads
+    move_closure();
+
+    // use channel to communicate
+    channel_part();
+
+    // multi producers by cloning the transmitter
+    multi_producers();
+
+    // mutex part
+    mutex_part();
+
+    mutex_multiple_threads();
+}
+
+
+fn basic_case() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
+
+
+fn move_closure() {
+    let v = vec![1, 2, 3];
+
+    // some awful case will happen
+    // if without move, thread want to use a reference to v,
+    // but v will be dropped when the main thread ends
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
+
+fn channel_part() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn multi_producers() {
+    let (tx, rx) = channel();
+
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+
+
+fn mutex_part() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+fn mutex_multiple_threads() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
